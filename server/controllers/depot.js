c2dd9f2a186097ee3de2c8955793825a37964311
Gedeon M. LEKOUNDA <gedeon.lekounda@gmail.com>
2023-09-22 14:16:40 +0100

    /bachelier-depot : [OK]

--- server/controllers/depot.js
+++ server/controllers/depot.js
@@ -31,23 +31,26 @@ exports.depotProfile1 = (req, res)=>{
             req.flash("message", "Veuillez remplir tout les champs, car ils sont essentiels") 
             return res.redirect('/inscription#ins')
         } 
-    pool.getConnection( (err, connexion) => {
+
+        const newcandidat = {
+            ins_nom,
+            ins_prenom,
+            ins_email,
+            ins_tel,
+            ins_profile,
+            ins_bac, 
+            ins_filiere, 
+            ins_cycle
+        }
+     pool.getConnection(async (err, connexion) =>  {
         if(err)
         {
             console.log('Erreur est survenue au niveau de la base de données ')
         }
-        pool.query(`INSERT INTO cficiras_website.cfi_candidat set 
-             ins_nom = ? , 
-             ins_prenom =? , 
-             ins_tel = ? , 
-             ins_bac= ? , 
-             ins_statut = ? ,
-             ins_filiere= ? , 
-             ins_cycle = ? `, 
-             [ins_nom,ins_prenom,ins_tel,ins_bac, ins_profile,ins_filiere,ins_cycle]) ;
-            connexion.release() ;
-            req.flash("success" , "Dossier validé avec succès, Rapprochez-vous de la scolarité du CFI-CIRAS pour la suite") 
-            console.log("Données enregistrer avec success sur la base de données")
-            res.send(json(req.body));
+        pool.query('INSERT INTO cfi_candidat set ? ', [newcandidat]) ;
+        uploadPath = './server/uploads/' +"./"+ins_nom+"__"+ins_prenom+'/' ;
+        // creation dossier comportant le nom+prenom du dépositaire
+        fs.promises.mkdir(uploadPath, { recursive: true })
+        return res.redirect('/inscription#bloc_inscription')
     })
 }
